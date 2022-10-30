8d3553af4421dbc59ecb5e3d3a77080140af42ae
Matt Harrison <mattharrison@Matts-MacBook-Pro.local>
2022-10-30 13:36:55 -0700

    first commit

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "X-BingApis-SDK": "true",
+  "X-RapidAPI-Key": "14bf6f9af9mshc475fcf0e74da81p158347jsn42c60cb6d971",
+  "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com"
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+        })
+    })
+});
+
+export const {
+    useGetCryptoNewsQuery,
+} = cryptoNewsApi;
+
+
